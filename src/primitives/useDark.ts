27a0c496f6ffb486a0e8a8a90219610a16e52d5b
WSRer <1749094641@qq.com>
2025-01-10 13:59:01 +0800

    update

--- src/primitives/useDark.ts
+++ src/primitives/useDark.ts
@@ -1,9 +1,12 @@
 import { usePrefersDark } from '@solid-primitives/media'
-import { createStorageSignal } from '@solid-primitives/storage'
+import { makePersisted } from '@solid-primitives/storage'
 
 export default function useDark() {
   const prefersDark = usePrefersDark()
-  const [value, setValue] = createStorageSignal<string>('dark-mode', 'auto', { api: localStorage })
+  const [value, setValue, init] = makePersisted(createSignal("dark-mode"), {name: 'mode', storage: localStorage})
+  if (value() !== 'auto' && value() !== 'dark') {
+    setValue('auto')
+  }
   const isDark = createMemo(() => value() === 'auto' ? prefersDark() : value() === 'dark')
 
   createEffect(() => document.documentElement.classList.toggle('dark', isDark()))
